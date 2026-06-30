a26118f005b4983a8d6232491d771d9ac7eae0a8
Akeem Ali <akeemchoudhary1856@gmail.com>
2026-06-30 16:02:54 +0530

    Remove build artifacts and add .gitignore

--- obj/Debug/net10.0/RetailInventory.AssemblyInfo.cs
+++ obj/Debug/net10.0/RetailInventory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetailInventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98aeed081952305f98e5282860c6dc97106dfe06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5c76b626280c063e948d0716e1c63c1bbdec203")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetailInventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetailInventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
